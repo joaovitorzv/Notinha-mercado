4e18cbc2787bc15741f0243e47ec0dd0e53beacc
Joao vitor O <34951179+Jaozera1@users.noreply.github.com>
2019-02-19 21:14:51 -0300

    notinha-version-1.1
    Adicionado sistema de saldo em conta.

--- notinha-version-01.py
+++ notinha-version-01.py
@@ -12,16 +12,23 @@ imposto = 10 / 100.0 * soma
 total = soma + imposto
 
 print("Sua compra deu: ", total)
-pgmto = int(input("Insira a quantidade que deseja cobrar: "))
-troco = pgmto - total
-
-#NOTINHA
-print("============SUPER=MERCADO=LAOR=========")
-print(item1,"..........................", pre1)
-print(item2,"..........................", pre2)
-print(item3,"..........................", pre3)
-print("----------------------------------------")
-print("valores de impostos 10%: ", imposto)
-print("              TOTAL R$: ", soma, "Reais")
-print("              Troco R$:", troco, "Reais")
-print("=======OBRIGADO PELA PREFENRENCIA=======")
+conta = 600
+saldo = conta - total
+if saldo <= total:
+    print("Saldo insuficiente :(")
+else:
+    input("Transação aprovada! Deseja finalizar (s/n)")
+s = True
+f = False
+if s:
+    print("============SUPER=MERCADO=LAOR=========")
+    print(item1,"..........................", pre1)
+    print(item2,"..........................", pre2)
+    print(item3,"..........................", pre3)
+    print("----------------------------------------")
+    print("valores de impostos 10%: ", imposto)
+    print("              TOTAL R$: ", soma, "Reais")
+    print("     Saldo em conta R$:", saldo, "Reais")
+    print("=======OBRIGADO PELA PREFENRENCIA=======")
+else:
+    print("Compra cancelada!!!")
